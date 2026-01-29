81c0f5662d4f41a961d82b28bbad2bb80b7df02a
ayush <ayushvyas99199@gmail.com>
2026-01-29 20:40:48 +0530

    m

--- Movie Recommendation system/backend.py
+++ Movie Recommendation system/backend.py
@@ -14,7 +14,7 @@ app = FastAPI(title="Movie Recommender API", version="1.0.0")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080/","http://localhost:8080", "http://localhost:5173", "http://localhost:3000"],  # Vite dev server
+    allow_origins=["*"],  # Vite dev server
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
